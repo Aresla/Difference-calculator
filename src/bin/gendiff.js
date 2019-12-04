3282e889fe3429efa06b114fed37faf57f986255
Aresla <areslas@list.ru>
2019-12-04 20:44:25 +0300

    add format option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,9 +4,16 @@ import commander from 'commander';
 
 const run = () => {
     const program = new commander.Command();
+    let firstConfig, secondConfig, type;
     program.version('0.0.1')
-        .arguments('<firstConfig> <secondConfig>')
+        .arguments('<firstConfig> <secondConfig> [type]')
         .description('Compares two configuration files and shows a difference.')
+        .option('-f, --format [type]', 'Output format')
+        .action(function (first, second, t) {
+            firstConfig = first;
+            secondConfig = second;
+            type = t;
+        })
         .parse(process.argv);
 
     return program;
